f0aa80e5f3d00ca3f3b863afeaf2cb70ae23f780
venkatesh raju <venkateshrajuknr@gmail.com>
2021-03-25 16:36:21 +0530

    Intial commit version1.0.01

---
+++ src/test/java/com/example/sampledemoapp/SampleDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampledemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
